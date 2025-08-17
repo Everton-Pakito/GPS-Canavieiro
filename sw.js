1a84ae7276d022c78215fa1b9298a4d24b661c7b
EVERTON TEZZON FERREIRA <128308115+Everton-Pakito@users.noreply.github.com>
2025-08-16 21:23:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-const CACHE='gpscanavieiro-v2';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./routes/sample.gpx'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+[SW_JS_CONTENT]
